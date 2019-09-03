f5bf72281c10fcdb2d5713ca0d4b6f0937ad19b1
Ameykyl <37608388+Ameykyl@users.noreply.github.com>
2019-09-03 08:26:36 +0800

    Update global.lua

--- files/usr/lib/lua/luci/model/cbi/koolproxy/global.lua
+++ files/usr/lib/lua/luci/model/cbi/koolproxy/global.lua
@@ -1,24 +1,31 @@
+-- Copyright 2018 Nick Peng (pymumu@gmail.com)
+
+require ("nixio.fs")
+require ("luci.http")
+require ("luci.dispatcher")
+require ("nixio.fs")
+
 local fs = require "nixio.fs"
 local sys = require "luci.sys"
 local http = require "luci.http"
 
+
 local o,t,e
 local v=luci.sys.exec("/usr/share/koolproxy/koolproxy -v")
-local s=luci.sys.exec("head -3 /usr/share/koolproxy/data/rules/koolproxy.txt | grep rules | awk -F' ' '{print $3,$4}'")
-local u=luci.sys.exec("head -4 /usr/share/koolproxy/data/rules/koolproxy.txt | grep video | awk -F' ' '{print $3,$4}'")
-local p=luci.sys.exec("head -3 /usr/share/koolproxy/data/rules/daily.txt | grep rules | awk -F' ' '{print $3,$4}'")
-local l=luci.sys.exec("grep -v !x /usr/share/koolproxy/data/rules/koolproxy.txt | wc -l")
-local q=luci.sys.exec("grep -v !x /usr/share/koolproxy/data/rules/daily.txt | wc -l")
+local s=luci.sys.exec("grep -v !x /usr/share/koolproxy/data/rules/easylistchina.txt | wc -l")
+local u=luci.sys.exec("grep -v !x /usr/share/koolproxy/data/rules/fanboy.txt | wc -l")
+local p=luci.sys.exec("grep -v !x /usr/share/koolproxy/data/rules/yhosts.txt | wc -l")
 local h=luci.sys.exec("grep -v '^!' /usr/share/koolproxy/data/rules/user.txt | wc -l")
 local i=luci.sys.exec("cat /usr/share/koolproxy/dnsmasq.adblock | wc -l")
 
 if luci.sys.call("pidof koolproxy >/dev/null") == 0 then
-	status = translate("<strong><font color=\"green\">KoolProxy is Running</font></strong>")
+	status = translate("<strong><font color=\"green\">KoolProxyR plus+  运行中</font></strong>")
 else
-	status = translate("<strong><font color=\"red\">KoolProxy is Not Running</font></strong>")
+	status = translate("<strong><font color=\"red\">KoolProxyR plus+  已停止</font></strong>")
 end
 
-o = Map("koolproxy", translate("KoolProxy"), translate("A powerful advertisement blocker. <br /><font color=\"red\">Adblock Plus Host list + koolproxy Blacklist mode runs without loss of bandwidth due to performance issues.<br /></font>"))
+o = Map("koolproxy", translate("KoolProxyR plus+ "), translate("KoolProxyR plus+是能识别adblock规则的免费开源软件,追求体验更快、更清洁的网络，屏蔽烦人的广告 <br /><font color=\"red\"><br /></font>"))
+
 
 t = o:section(TypedSection, "global")
 t.anonymous = true
@@ -52,12 +59,19 @@ e:value(3, translate("视频模式"))
 e = t:taboption("base", MultiValue, "koolproxy_rules", translate("内置规则"))
 e.optional = false
 e.rmempty = false
-e:value("koolproxy.txt", translate("静态规则"))
-e:value("daily.txt", translate("每日规则"))
+e:value("easylistchina.txt", translate("ABP规则"))
+e:value("fanboy.txt", translate("fanboy规则"))
+e:value("yhosts.txt", translate("yhosts规则"))
 e:value("kp.dat", translate("视频规则"))
 e:value("user.txt", translate("自定义规则"))
 
 e = t:taboption("base", ListValue, "koolproxy_port", translate("端口控制"))
+e.default = 0
+e.rmempty = false
+e:value(0, translate("关闭"))
+e:value(1, translate("开启"))
+
+e = t:taboption("base", ListValue, "koolproxy_ipv6", translate("IPv6支持(一刀切,有副作用)"))
 e.default = 0
 e.rmempty = false
 e:value(0, translate("关闭"))
@@ -68,7 +82,7 @@ e:depends("koolproxy_port", "1")
 e.rmempty = false
 e.description = translate(string.format("<font color=\"red\"><strong>单端口:80&nbsp;&nbsp;多端口:80,443</strong></font>"))
 
-e=t:taboption("base",Flag,"koolproxy_host",translate("开启Adblock Plus Host"))
+e=t:taboption("base",Flag,"koolproxy_host",translate("开启Adblock Plus Hosts"))
 e.default=0
 e:depends("koolproxy_mode","2")
 
@@ -77,9 +91,9 @@ e = t:taboption("base", ListValue, "koolproxy_acl_default", translate("默认访
 e.default = 1
 e.rmempty = false
 e:value(0, translate("不过滤"))
-e:value(1, translate("http only"))
-e:value(2, translate("http + https"))
-e:value(3, translate("full port"))
+e:value(1, translate("过滤HTTP协议"))
+e:value(2, translate("过滤HTTP(S)协议"))
+e:value(3, translate("全部过滤"))
 e.description = translate(string.format("<font color=\"blue\"><strong>访问控制设置中其他主机的默认规则</strong></font>"))
 
 e = t:taboption("base", ListValue, "time_update", translate("定时更新"))
@@ -88,7 +102,7 @@ for t = 0,23 do
 end
 e.default = 0
 e.rmempty = false
-e.description = translate(string.format("<font color=\"red\"><strong>定时更新订阅规则与Adblock Plus Host</strong></font>"))
+e.description = translate(string.format("<font color=\"red\"><strong>定时更新订阅规则与Adblock Plus Hosts</strong></font>"))
 
 e = t:taboption("base", Button, "restart", translate("规则状态"))
 e.inputtitle = translate("更新规则")
@@ -97,7 +111,7 @@ e.write = function()
 	luci.sys.call("/usr/share/koolproxy/kpupdate 2>&1 >/dev/null")
 	luci.http.redirect(luci.dispatcher.build_url("admin","services","koolproxy"))
 end
-e.description = translate(string.format("<font color=\"red\"><strong>更新订阅规则与Adblock Plus Host</strong></font><br /><font color=\"green\">静态规则: %s / %s条 视频规则: %s<br />每日规则: %s / %s条 自定义规则: %s条<br />Host: %s条</font><br />", s, l, u, p, q, h, i))
+e.description = translate(string.format("<font color=\"red\"><strong>更新订阅规则与Adblock Plus Hosts</strong></font><br /><font color=\"green\">ABP规则: %s条<br />fanboy规则: %s条<br />yhosts规则: %s条<br />自定义规则: %s条<br />Host: %s条</font><br />", s, u, p, h, i))
 t:tab("cert",translate("Certificate Management"))
 
 e=t:taboption("cert",DummyValue,"c1status",translate("<div align=\"left\">Certificate Restore</div>"))
@@ -117,11 +131,12 @@ if nixio.fs.access("/usr/share/koolproxy/data/certs/ca.crt")then
 	end
 end
 
+
 t:tab("white_weblist",translate("网站白名单设置"))
 
 local i = "/etc/adblocklist/adbypass"
 e = t:taboption("white_weblist", TextValue, "adbypass_domain")
-e.description = translate("These had been joined websites will not usefilter.Please input the domain names of websites,every line can input only one website domain.For example,google.com.")
+e.description = translate("这些已经加入的网站将不会使用过滤器。请输入网站的域名，每行只能输入一个网站域名。例如google.com。")
 e.rows = 28
 e.wrap = "off"
 e.rmempty = false
@@ -147,7 +162,7 @@ t:tab("weblist",translate("Set Backlist Of Websites"))
 
 local i = "/etc/adblocklist/adblock"
 e = t:taboption("weblist", TextValue, "adblock_domain")
-e.description = translate("These had been joined websites will use filter,but only blacklist model.Please input the domain names of websites,every line can input only one website domain.For example,google.com.")
+e.description = translate("加入的网址将走广告过滤端口。只针对黑名单模式。只能输入WEB地址，如：google.com，每个地址一行。")
 e.rows = 28
 e.wrap = "off"
 e.rmempty = false
@@ -173,7 +188,7 @@ t:tab("white_iplist",translate("IP白名单设置"))
 
 local i = "/etc/adblocklist/adbypassip"
 e = t:taboption("white_iplist", TextValue, "adbypass_ip")
-e.description = translate("These had been joined ip addresses will use proxy, but only GFW model.Please input the ip address or ip address segment,every line can input only one ip address.For example,112.123.134.145/24 or 112.123.134.145.")
+e.description = translate("这些已加入的ip地址将使用代理，但只有GFW型号。请输入ip地址或ip地址段，每行只能输入一个ip地址。例如，112.123.134.145 / 24或112.123.134.145。")
 e.rows = 28
 e.wrap = "off"
 e.rmempty = false
@@ -199,7 +214,7 @@ t:tab("iplist",translate("IP黑名单设置"))
 
 local i = "/etc/adblocklist/adblockip"
 e = t:taboption("iplist", TextValue, "adblock_ip")
-e.description = translate("These had been joined ip addresses will not use filter.Please input the ip address or ip address segment,every line can input only one ip address.For example,112.123.134.145/24 or 112.123.134.145.")
+e.description = translate("这些已经加入的ip地址不会使用filter.Please输入ip地址或ip地址段，每行只能输入一个ip地址。例如，112.123.134.145 / 24或112.123.134.145。")
 e.rows = 28
 e.wrap = "off"
 e.rmempty = false
@@ -263,7 +278,7 @@ end
 function e.write(self, section, value)
 end
 
-t=o:section(TypedSection,"acl_rule",translate("KoolProxy 访问控制"),
+t=o:section(TypedSection,"acl_rule",translate("KoolProxyR 访问控制"),
 translate("ACLs is a tools which used to designate specific IP filter mode,The MAC addresses added to the list will be filtered using https"))
 t.template="cbi/tblsection"
 t.sortable=true
@@ -298,7 +313,11 @@ e:value(1,translate("http only"))
 e:value(2,translate("http + https"))
 e:value(3,translate("full port"))
 
-t=o:section(TypedSection,"rss_rule",translate("KoolProxy 规则订阅"), translate("请确保订阅规则的兼容性"))
+
+
+
+
+t=o:section(TypedSection,"rss_rule",translate("KoolProxyR 规则订阅"), translate("请确保订阅规则的兼容性"))
 t.anonymous=true
 t.addremove=true
 t.sortable=true
@@ -367,4 +386,32 @@ function(o,a,i)
 	end
 end
 )
+
+t=o:section(TypedSection,"rss_rulee",translate("Technical Support"), 
+	translate("本软件由KPR提供技术支持"))
+
+t.anonymous = true
+
+e=t:option(Button, "webe")
+e.title = translate("koolproxyR开源项目")
+e.inputtitle = translate("点击前往")
+e.inputstyle = "apply"
+e.write = function()
+	luci.http.redirect("https://github.com/user1121114685/koolproxyR")
+end
+e=t:option(Button, "web")
+e.title = translate("koolproxyR使用说明")
+e.inputtitle = translate("点击前往")
+e.inputstyle = "apply"
+e.write = function()
+	luci.http.redirect("https://shaoxia.xyz/post/koolproxyr%E6%8C%87%E5%8D%97/")
+end
+e=t:option(Button, "Donate")
+e.title = translate("koolproxyR交流群")
+e.inputtitle = translate("加入telegram群")
+e.inputstyle = "apply"
+e.write = function()
+	luci.http.redirect("https://t.me/koolproxyR")
+end
+
 return o
